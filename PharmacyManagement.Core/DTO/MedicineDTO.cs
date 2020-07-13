a15f8c650c35ff340ce5a3fba4e67fdb352c7257
Rounak Hasan <hasan.rounak@gmail.com>
2020-07-13 11:39:57 +0530

    Color Logic Added

--- PharmacyManagement.Core/DTO/MedicineDTO.cs
+++ PharmacyManagement.Core/DTO/MedicineDTO.cs
@@ -11,5 +11,23 @@ namespace PharmacyManagement.Core.DTO
         public double Price { get; set; }
         public short Quantity { get; set; }
         public DateTime ExpiryDate { get; set; }
+        private string status;
+
+        public string Status
+        {
+            get
+            {
+                if (ExpiryDate.AddDays(-30)<DateTime.Now)
+                {
+                    status = "R";
+                }
+                else if (Quantity<10)
+                {
+                    status = "Y";
+                }
+                return status;
+            }
+        }
+
     }
 }
